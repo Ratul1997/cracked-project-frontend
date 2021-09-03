b2746fad5dc1346d0964a3f65c32a161bb359f51
MDAmir159 <islamamirul497@gmail.com>
2021-09-03 19:14:12 +0600

    whatever

--- src/Utility/index.js
+++ src/Utility/index.js
@@ -1,16 +1,25 @@
 export const isLoggedIn = () =>{
-    let value = localStorage.getItem('DLGT_PROJECT2_postGivingAppRemastered');
-    const obj = JSON.parse(value);
-    console.log(obj.loggedIn);
-    return obj.loggedIn;
-    // return false;
+    let value = localStorage.getItem('DLGT_PROJECT2_postGivingAppRemastered')
+    if(value){
+        const obj = JSON.parse(value);
+        console.log(obj.loggedIn);
+        return obj.loggedIn;
+    } else {
+        return false;
+    }    
 }
 
 export const getloggedInUsersInfo = () =>{
     let value = localStorage.getItem('DLGT_PROJECT2_postGivingAppRemastered');
-    const obj = JSON.parse(value);
-    console.log(obj.userDetails);
-    return obj.userDetails;
+    if(value){
+        const obj = JSON.parse(value);
+        console.log(obj.userDetails);
+        return obj.userDetails;
+    } else{
+        return false;
+    }
+    
+    
 }
 
 export const getLoggedIn = (key , value) =>{
